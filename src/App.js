bfa0a65575e6081b6e42be22e9b43dd5203d75e6
Nana K <73991180+topboyasante@users.noreply.github.com>
2022-12-28 10:26:19 +0000

    Adding Routes.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <main className='bg-[#000000]'>
       <Navbar/>
-      <Homepage/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+      </Routes>
     </main>
     )
 }
